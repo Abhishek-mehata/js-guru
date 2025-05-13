2c2b91597a761fe2c975df739a2153d3c331afc3
Abhishek-mehata <mehtaabishekkumar99gmail.com>
2025-05-13 13:51:29 +0545

    Fetch API

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// async function getData(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(()=>{
-//             resolve(455)
-//         },3000)
-//     })
-// }
-
-// console.log("Doing Task 1")
-
-// let data = getData()
-// console.log(data) // Promise { <pending> }
-
-// console.log("Doing Task 2")
-
+// Send a DELETE request to the given URL (to delete a resource)
+fetch("https://api.example.com/users/1", {
+    method: "DELETE"  // the DELETE method
+})
+    .then(response => {
+        // Check if the response status is OK (status code 200–299)
+        if (response.ok) {
+            console.log("Resource Deleted");  // If yes, resource was deleted successfully
+        } else {
+            console.log("Delete Operation Failed");  // If not, deletion failed
+        }
+    })
+    .catch(error => {
+        // If there’s any network error (like no internet or server unreachable), catch it here
+        console.error("Error deleting resource:", error);
+    });
