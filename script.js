654f2c9c460f34e706824dfa41e12e80e0e920a7
Abhishek-mehata <mehtaabishekkumar99gmail.com>
2025-06-19 09:00:35 +0545

    adding date docs

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-// Send a DELETE request to the given URL (to delete a resource)
-fetch("https://api.example.com/users/1", {
-    method: "DELETE"  // the DELETE method
-})
-    .then(response => {
-        // Check if the response status is OK (status code 200–299)
-        if (response.ok) {
-            console.log("Resource Deleted");  // If yes, resource was deleted successfully
-        } else {
-            console.log("Delete Operation Failed");  // If not, deletion failed
-        }
-    })
-    .catch(error => {
-        // If there’s any network error (like no internet or server unreachable), catch it here
-        console.error("Error deleting resource:", error);
-    });
+async function getPost() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/1")
+        const data = await response.json()
+        // console.log(")
+        console.log(data)
+    } catch (error) {
+        console.log("Error captured: ", error)
+    }
+}
+
+getPost()
