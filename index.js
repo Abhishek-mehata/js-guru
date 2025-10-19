cda40dcffc5f4b1ff1d9e8ea0c40a6c841ba3e15
Abhishek-mehata <mehtaabishekkumar99gmail.com>
2025-10-19 10:09:27 +0545

    async

--- index.js
+++ index.js
@@ -1,14 +1,88 @@
-import { greet, print } from "./utils.js"
-// importing the functions from utils.js file with named export
+// // // promise chaining
+// // Promise chaining allows you to execute a series of asynchronous operations in sequence.
+// // // Promise chaining means calling multiple .then() handlers one after another,
+// // //  where the result of one .then() is passed to the next.
 
 
-let isMathRequired = true;
-if (isMathRequired) {
-    // dynamic import
-    // await import("./math.js");
+// // new Promise((resolve, reject) => {
+// //     setTimeout(() => resolve(10), 1000);
+// // })
+// //     .then((num) => {
+// //         console.log("1.Got:", num);
+// //         return num * 2; // passes 20 to next then
+// //     })
+// //     .then((num) => {
+// //         console.log("2.Got:", num);
+// //         return num * 3; // passes 60 to next then
+// //     })
+// //     .then((num) => {
+// //         console.log("3.Got:", num);
+// //     });
 
-    const { add } = await import("./math.js")
 
-    console.log("Dynamic Import: 5 + 10 =", add(5, 10));
-}
+// // // Each .then() returns a new promise, which allows the next .then() to wait for the previous one.
+
+
+
+
+
+// function fetchUser() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve({ id: 1, name: "Abhishek" }), 1000);
+//     });
+// }
+
+// function fetchPosts(userId) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve(["Post 1", "Post 2"]), 1000);
+//     });
+// }
+
+// fetchUser()
+//     .then((user) => {
+//         console.log("User:", user);
+//         return fetchPosts(user.id); // returns another promise
+//     })
+//     .then((posts) => {
+//         console.log("Posts:", posts);
+//     })
+//     .catch((err) => {
+//         console.error("Error:", err);
+//     });
+// // fetchUser() resolves → gives user info
+// // That result goes to next .then()
+// // fetchPosts() runs → returns new promise
+// // Next .then() handles posts
+// // .catch() handles any error in the chain
+
+
+
+
+
+
+// // ----------------
+
+// function task(message, delay) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => {
+//             console.log(message);
+//             resolve();
+//         }, delay);
+//     });
+// }
+
+// // Chaining promises
+// task('Task 1 completed', 1000)
+//     .then(() => task('Task 2 completed', 2000))
+//     .then(() => task('Task 3 completed', 1000));
+
+
+
+
+
+// ----------------------------------------------------------------------------------------------------------------------------------------
+// Promise APIs
+
+
+// Promise.all([arrayOfPromises])
 
--- math.js
+++
@@ -1,9 +0,0 @@
-export const add = (x, y) => x + y;
-// export const subtract = (x, y) => x - y;
-// export const multiply = (x, y) => x * y;
-// export const divide = (x, y) => x / y;
-// export const square = (x) => x * x;
-// export const cube = (x) => x * x * x;
-// export const power = (x, y) => Math.pow(x, y);
-// export const sqrt = (x) => Math.sqrt(x);
-// export const cbrt = (x) => Math.cbrt(x);
